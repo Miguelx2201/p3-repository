19db355c17d2bb1d3f3998a0baf78f35b209b47e
Miguel Gutierrez <munekoporsiempre2017@gmail.com>
2026-09-08 15:38:46 -0500

    moddify the util and delete a trash file

--- problem001/util.ex
+++ problem001/util.ex
@@ -1,6 +1,10 @@
 defmodule Util do
 
   def mostrar_mensaje(mensaje) do
+    IO.puts(mensaje);
+  end
+
+  def mostrar_mensaje_python(mensaje) do
     System.cmd("python3", ["mostrar_dialogo.py", mensaje])
   end
   def mostrar_mensaje_java(mensaje) do
