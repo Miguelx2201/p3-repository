26adad81b16c4e88e8c0bf94ecb1a7263fab4ac2
Miguel Gutierrez <munekoporsiempre2017@gmail.com>
2026-09-10 19:18:08 -0500

    estructura ejercicio
    estructura inicial del archivo para el ultimo ejercicio, falta realizarlo

---
+++ problem004/tienda.exs
@@ -0,0 +1,16 @@
+Code.require_file("../utilities/util.ex", __DIR__)
+alias Util
+
+defmodule Tienda do
+  @moduledoc """
+  Modulo que representa los descuentos que le hace una tienda a sus clientes.
+  V1.0
+  Autor: Juan Miguel Gutierrez Vargas
+  10 de Septiembre de 2026
+  """
+  def main do
+
+  end
+end
+
+Tienda.main()
