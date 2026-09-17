c955a4d59ed183d84ba765cc409111d6a99f7fd3
Miguel Gutierrez <munekoporsiempre2017@gmail.com>
2026-09-17 17:11:40 -0500

    using cond, guards

--- problem005/descuento.exs
+++ problem005/descuento.exs
@@ -9,20 +9,20 @@ defmodule Descuento do
     |> generar_mensaje()
     |> Util.mostrar_mensaje_java()
   end
-
-  defp calcular_precio(precio) do
-    unless precio > 50000 do
-      precio
-    else unless precio > 100000 do
-      precio*0.95
-    else unless precio > 500000 do
-      precio*0.9
-    else
-      precio*0.85
-    end
-    end
-    end
-  end
+  #Metodo usando cond -> 1 clausula
+  # defp calcular_precio(precio) do
+  #   cond do
+  #     precio < 50000 -> precio
+  #     precio < 100000 -> precio*0.95
+  #     precio < 500000 -> precio*0.9
+  #     precio > 500000 -> precio*0.85
+  #   end
+  # end
+  #Metodo usando guardas (when) -> 4 clausulas
+  defp calcular_precio(precio) when precio < 50000, do: precio*1
+  defp calcular_precio(precio) when precio < 100000, do: precio*0.95
+  defp calcular_precio(precio) when precio < 500000, do: precio*0.9
+  defp calcular_precio(precio) when precio >= 500000, do: precio*0.85
 
   defp generar_mensaje(precio) do
     "El precio final de la compra es de #{Util.formatter(precio)}"
